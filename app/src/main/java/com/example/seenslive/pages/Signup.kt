e36e29f11602ffdc2a1fbd1e0bbea2fad7ac92b6
IamBansal <abansal11ae@gmail.com>
2022-08-08 16:36:10 +0530

    Login Screen Added

---
+++ app/src/main/java/com/example/seenslive/pages/Signup.kt
@@ -0,0 +1,12 @@
+package com.example.seenslive.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.seenslive.R
+
+class Signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
