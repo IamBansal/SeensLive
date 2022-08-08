0081128a4dc6c1189b7808afcdbebce5630cc8d3
IamBansal <abansal11ae@gmail.com>
2022-08-08 17:15:34 +0530

    Signup Screen Added

--- app/src/main/java/com/example/seenslive/pages/Signup.kt
+++ app/src/main/java/com/example/seenslive/pages/Signup.kt
@@ -1,12 +1,24 @@
 package com.example.seenslive.pages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.seenslive.R
 
 class Signup : AppCompatActivity() {
+
+    private lateinit var login : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        login = findViewById(R.id.tvAlreadyUser)
+
+        login.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
+
     }
 }
