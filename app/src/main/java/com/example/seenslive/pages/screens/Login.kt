f190d2f2ba9695e808f7f262c22955a7c59b656d
IamBansal <abansal11ae@gmail.com>
2022-08-21 15:19:16 +0530

    UI Fix

--- app/src/main/java/com/example/seenslive/pages/screens/Login.kt
+++ app/src/main/java/com/example/seenslive/pages/screens/Login.kt
@@ -3,21 +3,28 @@ package com.example.seenslive.pages.screens
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.seenslive.R
 
 class Login : AppCompatActivity() {
 
     private lateinit var signUp : TextView
+    private lateinit var login : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         signUp = findViewById(R.id.tvNewUser)
+        login = findViewById(R.id.btnSignIn)
 
         signUp.setOnClickListener {
             startActivity(Intent(this, Signup::class.java))
+        }
+
+        login.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
         }
 
 
